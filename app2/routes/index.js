a38b6dd37c2d108900f1e09bc4f3e85d5c9bef24
Naresh Krish <narioinc89@gmail.com>
2022-03-17 17:21:32 +0530

    First commit -
    Adds two services app1 and app2 to showcase mtls enablement. Dockerfiles and K8s Deployments also included

---
+++ app2/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var axios =  require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send({"status": "success"});
+});
+
+router.get('/ipgeo', function(req, res, next) {
+  const ipRes = axios.get('https://ipinfo.io/161.185.160.93/geo').then(function (response) {
+    console.log(response.data);
+    res.send(response.data);
+  });  
+});
+
+router.get('/app1', function(req, res, next) {
+  const ipRes = axios.get('https://app1').then(function (response) {
+    console.log(response.data);
+    res.send(response.data);
+  });  
+});
+
+module.exports = router;
